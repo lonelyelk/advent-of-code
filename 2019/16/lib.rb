49993bf208417094d6fa6f9de882339faf652e25
Sergey Kruk <sergey.kruk@gmail.com>
2019-12-17 00:09:03 +0100

    Use the flaw in the flawed algorithm to solve p2 of day 16

--- 2019/16/lib.rb
+++ 2019/16/lib.rb
@@ -16,10 +16,32 @@ def fft_phase(input, offset = 0)
   input[0...offset] + arr
 end
 
+def fft_phase_v2(input, offset)
+  out = Array.new(input.length)
+  help = Array.new(input.length)
+  count = input.length - 1
+  out[input.length - 1] = help[input.length - 1] = input.last
+  while count >= offset
+    count -= 1
+    help[count] = help[count + 1] + input[count]
+    out[count] = help[count].digits.first
+  end
+  out
+end
+
 def fft_apply(input, phases, offset = 0)
   re = input
   phases.times do |i|
     re = fft_phase(re, offset)
+  end
+  re[offset, 8].join
+end
+
+def fft_apply_v2(input, phases, offset)
+  raise "ERROR" if offset < input.length / 2
+  re = input
+  phases.times do |i|
+    re = fft_phase_v2(re, offset)
   end
   re[offset, 8].join
 end
--- 2019/16/run.rb
+++ 2019/16/run.rb
@@ -2,5 +2,7 @@ require_relative "lib"
 
 input = File.read("input.txt").chomp.chars.map(&:to_i)
 
+# puts input.length * 10000.0 / input[0...7].join.to_i
+# exit
 puts fft_apply(input, 100)
-puts fft_apply(input * 10000, 100)[0...8].join
+puts fft_apply_v2(input * 10000, 100, input[0...7].join.to_i)
