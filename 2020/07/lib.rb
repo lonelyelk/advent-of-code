d1990353b0617b73bb474028a254654276a90caa
Sergey Kruk <sergey.kruk@gmail.com>
2020-12-07 09:53:53 +0100

    Refactor and format

--- 2020/07/lib.rb
+++ 2020/07/lib.rb
@@ -1,7 +1,8 @@
 def parse_rules(rules)
   rules.each_with_object({}) do |rule, set|
     md = rule.match(/^(\w+ \w+) bags/)
-    set[md[1]] = rule.scan(/(\d+) (\w+ \w+) bags?[.,]/).each_with_object({}) { |(num, b), s| s[b] = num.to_i }
+    set[md[1]] =
+      rule.scan(/(\d+) (\w+ \w+) bags?[.,]/).each_with_object({}) { |(num, b), s| s[b] = num.to_i }
   end
 end
 
@@ -10,12 +11,14 @@ def bag_found?(parsed_rules, rule, bag, starting_bag = nil)
 
   return true if rule.key?(bag)
 
-  rule.keys.reject { |b| b == starting_bag }.any? { |b| bag_found?(parsed_rules, parsed_rules[b], bag, starting_bag)}
+  rule.keys.any? do |b|
+    b != starting_bag && bag_found?(parsed_rules, parsed_rules[b], bag, starting_bag)
+  end
 end
 
 def outer_bag_count(parsed_rules, bag)
-  parsed_rules.keys.reject { |b| b == bag }.select do |b|
-    bag_found?(parsed_rules, parsed_rules[b], bag, b)
+  parsed_rules.keys.select do |b|
+    b != bag && bag_found?(parsed_rules, parsed_rules[b], bag, b)
   end.length
 end
 
