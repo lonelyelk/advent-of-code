da6e5e40008bf03d01d6a4c7a3000dea355cf991
Sergey Kruk <sergey.kruk@gmail.com>
2020-12-16 20:35:03 +0100

    Solve puzzle 2 of day 16

--- 2020/16/lib.rb
+++ 2020/16/lib.rb
@@ -25,23 +25,33 @@ def parse_ticket(rules_str, nearby_tickets, my_ticket)
   tickets = nearby_tickets.map { |str| str.split(",").map(&:to_i) }.select do |t|
     t.all? { |num| ranges.any? { |r| r.include?(num) } }
   end
-  puts tickets.inspect
-  puts rules.inspect
   ticket = my_ticket.split(",").map(&:to_i)
-  rules.keys.permutation.each do |rules_perm|
-    puts rules_perm.inspect
-    fit = tickets.all? do |t|
-      re = rules_perm.each_with_index.all? do |rule, index|
-        rules[rule].any? do |range|
-          range.include?(t[index])
-        end
-      end
-      re
-    end
-    if fit
-      return rules_perm.each_with_index.each_with_object({}) do |(rule, index), obj|
-        obj[rule] = ticket[index]
+  possible_matches = rules.each_with_object({}) do |(rule, ranges), match|
+    match[rule] = (0...tickets.first.length).each_with_object([]) do |index, arr|
+      if tickets.all? { |t| ranges.any? { |r| r.include?(t[index]) } }
+        arr.push(index)
       end
     end
   end
+  matches, solution = optimize(possible_matches)
+  raise "Can't solve" unless matches.empty?
+
+  solution.each_with_object({}) do |(rule, index), res|
+    res[rule] = ticket[index]
+  end
+end
+
+def optimize(possible_matches, prev_solution = {})
+  one = possible_matches.select { |_k, v| v.length == 1 }
+  if one.empty?
+    return [possible_matches, prev_solution]
+  end
+
+  next_sol = one.each_with_object(prev_solution) do |(k, v), sol|
+    sol[k] = v.first
+  end
+  next_matches = possible_matches.each_with_object({}) do |(k, v), mtch|
+    mtch[k] = v - one.values.first unless one.key?(k)
+  end
+  optimize(next_matches, next_sol)
 end
--- 2020/16/run.rb
+++ 2020/16/run.rb
@@ -10,4 +10,10 @@ index = instructions.index("your ticket:")
 my_ticket = instructions[index+1]
 
 puts invalid_sum(rules, nearby_tickets)
-puts parse_ticket(rules, nearby_tickets, my_ticket)
+puts(parse_ticket(rules, nearby_tickets, my_ticket).inject(1) do |mul, (rule, val)|
+  if rule.to_s.match(/^departure/)
+    mul * val
+  else
+    mul
+  end
+end)
