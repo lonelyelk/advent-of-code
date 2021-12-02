2f862a8013b9327b55aec4786b1c6b53c3c091e6
Sergey Kruk <sergey.kruk@gmail.com>
2021-12-02 11:53:32 +0100

    Autocorrect rubocop

--- 2021/02/lib.rb
+++ 2021/02/lib.rb
@@ -3,7 +3,7 @@
 module Day02
   def process_input(str)
     str.split("\n").reject(&:empty?).map do |l|
-      cmd, num = l.split(" ")
+      cmd, num = l.split
       [cmd, num.to_i]
     end
   end
@@ -24,11 +24,11 @@ module Day02
   end
 
   def problem2(input)
-    position, depth, _ = input.inject([0, 0, 0]) do |s, instr|
+    position, depth, = input.inject([0, 0, 0]) do |s, instr|
       pos, depth, aim = s
       case instr
       in ["forward", num]
-        [pos + num, depth + aim * num, aim]
+        [pos + num, depth + (aim * num), aim]
       in ["up", num]
         [pos, depth, aim - num]
       in ["down", num]
