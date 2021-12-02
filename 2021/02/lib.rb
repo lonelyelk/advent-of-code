01e513529a3640f659ce139735f14d0d721ff7f4
Sergey Kruk <sergey.kruk@gmail.com>
2021-12-02 11:49:02 +0100

    Solve problem 1 of day 2

---
+++ 2021/02/lib.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Day02
+  def process_input(str)
+    str.split("\n").reject(&:empty?).map do |l|
+      cmd, num = l.split(" ")
+      [cmd, num.to_i]
+    end
+  end
+
+  def problem1(input)
+    position, depth = input.inject([0, 0]) do |s, instr|
+      pos, depth = s
+      case instr
+      in ["forward", num]
+        [pos + num, depth]
+      in ["up", num]
+        [pos, depth - num]
+      in ["down", num]
+        [pos, depth + num]
+      end
+    end
+    position * depth
+  end
+
+  def problem2(input)
+  end
+end
---
+++ spec/2021/02/day02_lib_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require_relative "../../../2021/02/lib"
+
+RSpec.describe Day02 do
+  include described_class
+
+  let(:input) do
+    %(
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+)
+  end
+  let(:processed_input) do
+    [
+      ["forward", 5],
+      ["down", 5],
+      ["forward", 8],
+      ["up", 3],
+      ["down", 8],
+      ["forward", 2],
+    ]
+  end
+
+  describe "process_input" do
+    it "consumes multiline string and returns an array of ints" do
+      expect(process_input(input)).to eq(processed_input)
+    end
+  end
+
+  describe "problem1" do
+    it "returns 150 for test input" do
+      expect(problem1(processed_input)).to eq(150)
+    end
+  end
+
+  describe "problem2" do
+    it "returns 5 for test input" do
+      expect(problem2(processed_input)).to eq(nil)
+    end
+  end
+end
