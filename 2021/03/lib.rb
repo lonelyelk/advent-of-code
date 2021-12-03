7733dac0b714e3f67c9cce453d17eb2d1464d506
Sergey Kruk <sergey.kruk@gmail.com>
2021-12-03 13:47:02 +0100

    Refactor to pass rubocop

--- 2021/03/lib.rb
+++ 2021/03/lib.rb
@@ -1,64 +1,40 @@
 # frozen_string_literal: true
 
+# https://adventofcode.com/2021/day/3
 module Day03
   def process_input(str)
     str.split("\n").reject(&:empty?)
   end
 
   def gamma_rate(input)
-    counts = input.each_with_object(Array.new(input.first.size, 0)) do |val, counter|
-      val.chars.each_with_index { |bit, index| counter[index] += bit.to_i }
-    end
-    counts.each_with_index.inject(0) do |res, (bits, index)|
+    ones = count_ones(input)
+    ones.each_with_index.inject(0) do |res, (bits, index)|
       bit = bits > input.size / 2 ? 1 : 0
-      res |= bit << (counts.length - index - 1)
+      res | (bit << (ones.length - index - 1))
     end
   end
 
   def problem1(input)
     gamma = gamma_rate(input)
-    epsilon = (2 ** input.first.size - 1) ^ gamma
+    epsilon = ((2**input.first.size) - 1) ^ gamma
     gamma * epsilon
   end
 
   def oxygen_rate(input)
-    index = 0
-    filtered = input.dup
-    while filtered.size > 1
-      count = filtered.inject(0) { |cnt, val| cnt += val[index].to_i }
-      filtered.reject! do |val|
-        half = filtered.size / 2
-        reject_bit =
-          if filtered.size % 2 == 0 && count == half
-            "0"
-          elsif count > half
-            "0"
-          else
-            "1"
-          end
-        val[index] == reject_bit
-      end
-      index += 1
-    end
-    filtered.first.to_i(2)
+    gas_rate(input, %w[0 1])
   end
 
   def co2_rate(input)
+    gas_rate(input, %w[1 0])
+  end
+
+  def gas_rate(input, rejection_priority)
     index = 0
     filtered = input.dup
     while filtered.size > 1
-      count = filtered.inject(0) { |cnt, val| cnt += val[index].to_i }
+      ones = filtered.inject(0) { |cnt, val| cnt + val[index].to_i }
       filtered.reject! do |val|
-        half = filtered.size / 2
-        reject_bit =
-          if filtered.size % 2 == 0 && count == half
-            "1"
-          elsif count > half
-            "1"
-          else
-            "0"
-          end
-        val[index] == reject_bit
+        val[index] == reject_bit(filtered.size, ones, rejection_priority)
       end
       index += 1
     end
@@ -67,5 +43,22 @@ module Day03
 
   def problem2(input)
     oxygen_rate(input) * co2_rate(input)
+  end
+
+  protected
+
+  def count_ones(input)
+    input.each_with_object(Array.new(input.first.size, 0)) do |val, counter|
+      val.chars.each_with_index { |bit, index| counter[index] += bit.to_i }
+    end
+  end
+
+  def reject_bit(size, count, rejection_priority)
+    half = size / 2
+    if size.even? && count == half
+      rejection_priority[0]
+    else
+      count > half ? rejection_priority[0] : rejection_priority[1]
+    end
   end
 end
