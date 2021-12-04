8f48451d3025a91363d019bccee333462de0121d
Sergey Kruk <sergey.kruk@gmail.com>
2021-12-04 14:05:10 +0100

    Refactor to pass rubocop

--- 2021/04/lib.rb
+++ 2021/04/lib.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# https://adventofcode.com/2021/day/4
 module Day04
   def process_input(str)
     numbers, *boards = str.split("\n\n").reject(&:empty?)
@@ -13,27 +14,39 @@ module Day04
   def problem1(input)
     numbers, *boards = input
     numbers.each do |num|
-      boards = boards.map do |board|
-        board.map { |rc| rc.reject { |n| n == num } }
-      end
-      winning_board = boards.detect { |board| board.any?(&:empty?) }
-      return winning_board.reject(&:empty?).inject(0) { |acc, row| acc + row.inject(&:+) } * num / 2 if winning_board
+      boards = mark_number(boards, num)
+      winner = winner_score(boards, num)
+      return winner if winner
     end
   end
 
   def problem2(input)
     numbers, *boards = input
     numbers.each do |num|
-      boards = boards.map do |board|
-        board.map { |rc| rc.reject { |n| n == num } }
-      end
+      boards = mark_number(boards, num)
       if boards.size > 1
         boards = boards.reject { |board| board.any?(&:empty?) }
-      elsif boards.first.any?(&:empty?)
-        return boards.first.reject(&:empty?).inject(0) do |acc, row|
-                 acc + row.inject(&:+)
-               end * num / 2
+      else
+        winner = winner_score(boards, num)
+        return winner if winner
       end
     end
+  end
+
+  protected
+
+  def mark_number(boards, num)
+    boards.map do |board|
+      board.map { |rc| rc.reject { |n| n == num } }
+    end
+  end
+
+  def winner_score(boards, num)
+    winning_board = boards.detect { |board| board.any?(&:empty?) }
+    return score(winning_board, num) if winning_board
+  end
+
+  def score(board, num)
+    board.reject(&:empty?).inject(0) { |acc, row| acc + row.inject(&:+) } * num / 2
   end
 end
