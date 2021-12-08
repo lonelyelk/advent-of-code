c044782559ae9c06dda9b495eae59eb26fae1f4f
Sergey Kruk <sergey.kruk@gmail.com>
2021-12-08 14:46:05 +0100

    Refactor day 8 before running rubocop

--- 2021/08/lib.rb
+++ 2021/08/lib.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# https://adventofcode.com/2021/day/8
 module Day08
   LETTERS = "abcdefg".chars.freeze
   DIGITS = {
@@ -14,6 +15,11 @@ module Day08
     8 => "abcdefg".chars.freeze,
     9 => "abcdfg".chars.freeze,
   }.freeze
+  FREQUENCY = {
+    "e" => 4,
+    "b" => 6,
+    "f" => 9,
+  }.freeze
 
   def process_input(str)
     str.split("\n").reject(&:empty?).map { |l| l.split(/\s+\|\s+/).map(&:split) }
@@ -24,38 +30,51 @@ module Day08
   end
 
   def problem2(input)
-    frequency = DIGITS.each_with_object(Hash.new(0)) { |(_, l), acc| l.each { |l| acc[l] += 1 } }
-    unique_frequency = frequency.reject { |_, l| frequency.values.count(l) > 1 }
     input.inject(0) do |sum, line|
-      scrabled_frequency = line.first.each_with_object(Hash.new(0)) { |repr, acc| repr.chars.each { |l| acc[l] += 1 } }
-      dict = unique_frequency.each_with_object({}) { |(letter, freq), acc| acc[scrabled_frequency.key(freq)] = [letter] }
-      dict = line.first.each_with_object(dict) do |digit_repr, dict|
-        possible_letters = DIGITS.inject([]) { |acc, (_d, l)| l.size == digit_repr.size ? acc + l : acc }
-        repr_chars = digit_repr.chars
-        next if repr_chars.any? { |l| dict[l] && !dict[l].intersect?(possible_letters) }
-        repr_chars.each do |l|
-          if dict[l]
-            dict[l] = dict[l].intersection(possible_letters)
-          else
-            dict[l] = possible_letters
-          end
-        end
-      end
-      while dict.values.any? { |v| v.size > 1 }
-        dict = dict.each_with_object({}) do |(letter, vals), acc|
-          if vals.size == 1
-            acc[letter] = vals
-          else
-            acc[letter] = dict.values.inject(vals) { |a, v| v.size == 1 ? a - v : a }
-          end
-        end
-      end
-      dict = dict.each_with_object({}) { |(letter, vals), acc| acc[letter] = vals.first }
+      dict = build_dict(line.first)
+      dict = deduce(dict)
       number = line.last.map do |repr|
         mapped = repr.chars.map { |c| dict[c] }
         DIGITS.keys.detect { |d| DIGITS[d].size == mapped.size && DIGITS[d].difference(mapped).empty? }
       end
       sum + number.join.to_i
     end
+  end
+
+  protected
+
+  def segment_frequency(all_digits)
+    all_digits.each_with_object(Hash.new(0)) { |repr, acc| repr.chars.each { |letter| acc[letter] += 1 } }
+  end
+
+  def build_dict(signals)
+    frequency = segment_frequency(signals)
+    dict = FREQUENCY.each_with_object({}) { |(letter, freq), acc| acc[frequency.key(freq)] = [letter] }
+    signals.each_with_object(dict) do |digit_repr, dict|
+      possible_letters = DIGITS.inject([]) { |acc, (_d, l)| l.size == digit_repr.size ? acc + l : acc }
+      repr_chars = digit_repr.chars
+      next if repr_chars.any? { |l| dict[l] && !dict[l].intersect?(possible_letters) }
+
+      repr_chars.each do |l|
+        if dict[l]
+          dict[l] = dict[l].intersection(possible_letters)
+        else
+          dict[l] = possible_letters
+        end
+      end
+    end
+  end
+
+  def deduce(dict)
+    while dict.values.any? { |v| v.size > 1 }
+      dict = dict.each_with_object({}) do |(letter, vals), acc|
+        if vals.size == 1
+          acc[letter] = vals
+        else
+          acc[letter] = dict.values.inject(vals) { |a, v| v.size == 1 ? a - v : a }
+        end
+      end
+    end
+    dict.each_with_object({}) { |(letter, vals), acc| acc[letter] = vals.first }
   end
 end
