270983fee7ffc2ab0cc9ad34bdfb346286720c47
Sergey Kruk <sergey.kruk@gmail.com>
2021-12-15 23:32:06 +0100

    Refactor to pass rubocop, but not for performance :)

--- 2021/15/lib.rb
+++ 2021/15/lib.rb
@@ -7,10 +7,7 @@ module Day15
   end
 
   def problem1(input)
-    total_risk = Array.new(input.size) do |i|
-      Array.new(input[i].size, Float::INFINITY)
-    end
-    total_risk[0][0] = 0
+    total_risk = init_total_risk(input)
     to_update = [[0, 0]]
     until to_update.empty?
       to_update = to_update.inject([]) do |acc, (x, y)|
@@ -21,41 +18,54 @@ module Day15
     total_risk[input.size - 1][input.first.size - 1]
   end
 
+  # it takes 1m 3s to finish, but I refuse to refactor it for now
   def problem2(input)
     real_input = input.map do |l|
       (0...5).to_a.inject([]) do |acc, i|
-        acc + l.map do |n|
-          n_n = n + i
-          n_n -= 9 if n_n > 9
-          n_n
-        end
+        acc + rotate_line(l, i)
       end
     end
     real_input = (0...5).to_a.inject([]) do |acc, i|
-      acc + real_input.map do |l|
-        l.map do |n|
-          n_n = n + i
-          n_n -= 9 if n_n > 9
-          n_n
-        end
-      end
+      acc + real_input.map { |l| rotate_line(l, i) }
     end
     problem1(real_input)
   end
 
   protected
 
-  # rubocop:disable Naming/MethodParameterName
-  def update_neighbours(x, y, risk, total_risk)
-    to_update = []
-    [[x, y - 1], [x, y + 1], [x - 1, y], [x + 1, y]].each do |xx, yy|
-      next if xx.negative? || yy.negative? || xx >= risk.size || yy >= risk.first.size
-      next if total_risk[xx][yy] <= total_risk[x][y] + risk[xx][yy]
+  def init_total_risk(input)
+    total_risk = Array.new(input.size) do |i|
+      Array.new(input[i].size, Float::INFINITY)
+    end
+    total_risk[0][0] = 0
+    total_risk
+  end
 
-      total_risk[xx][yy] = total_risk[x][y] + risk[xx][yy]
+  # rubocop:disable Naming/MethodParameterName
+  def out_of_bounds?(x, y, field)
+    x.negative? || y.negative? || x >= field.size || y >= field.first.size
+  end
+
+  def neighbours(x, y, field)
+    [[x, y - 1], [x, y + 1], [x - 1, y], [x + 1, y]].reject { |xx, yy| out_of_bounds?(xx, yy, field) }
+  end
+
+  def update_neighbours(x, y, risk, total_risk)
+    neighbours(x, y, risk).each_with_object([]) do |(xx, yy), to_update|
+      potential = total_risk[x][y] + risk[xx][yy]
+      next if total_risk[xx][yy] <= potential
+
+      total_risk[xx][yy] = potential
       to_update.push([xx, yy])
     end
-    to_update
   end
   # rubocop:enable Naming/MethodParameterName
+
+  def rotate_line(line, offset)
+    line.map do |num|
+      next_num = num + offset
+      next_num -= 9 if next_num > 9
+      next_num
+    end
+  end
 end
