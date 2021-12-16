144218366e7afad92842bbd1f1faa041c1bd27d0
Sergey Kruk <sergey.kruk@gmail.com>
2021-12-16 21:52:21 +0100

    Revert "Minor refactoring and autofix rubocop"
    This reverts commit 6719a329ee69c665fe44d40ec3662becc3780ec0.

--- 2021/16/lib.rb
+++ 2021/16/lib.rb
@@ -3,7 +3,7 @@
 # https://adventofcode.com/2021/day/16
 module Day16
   def process_input(str)
-    [str.chomp].pack("H*").unpack1("B*")
+    [str.chomp].pack("H*").unpack("B*").first
   end
 
   def problem1(input)
@@ -20,15 +20,16 @@ module Day16
 
   def process_packet_version(input, pointer)
     version = input[pointer, 3].to_i(2)
-    pointer += 6
     case input[pointer + 3, 3].to_i(2)
     when 4
+      pointer += 6
       bits = input[pointer..].chars.each_slice(5).take_while { |s| s.first == "1" }.map { |s| s[1..].join }
       pointer += bits.size * 5
       bits.push(input[(pointer + 1), 4])
       pointer += 5
       [version, pointer]
     else
+      pointer += 6
       if input[pointer] == "0"
         len = input[pointer + 1, 15].to_i(2)
         pointer += 16
@@ -41,7 +42,7 @@ module Day16
       else
         num = input[pointer + 1, 11].to_i(2)
         pointer += 12
-        new_version, pointer = (0...num).to_a.inject([version, pointer]) do |(vn, pt), _i|
+        new_version, pointer = (0...num).to_a.inject([version, pointer]) do |(vn, pt), i|
           new_vn, new_pt = process_packet_version(input, pt)
           [vn + new_vn, new_pt]
         end
@@ -52,15 +53,16 @@ module Day16
 
   def process_packet(input, pointer)
     type = input[pointer + 3, 3].to_i(2)
-    pointer += 6
     case type
     when 4
+      pointer += 6
       bits = input[pointer..].chars.each_slice(5).take_while { |s| s.first == "1" }.map { |s| s[1..].join }
       pointer += bits.size * 5
       bits.push(input[(pointer + 1), 4])
       pointer += 5
       [bits.join.to_i(2), pointer]
     else
+      pointer += 6
       if input[pointer] == "0"
         len = input[pointer + 1, 15].to_i(2)
         pointer += 16
@@ -83,17 +85,17 @@ module Day16
       when 0
         [values.inject(&:+), pointer]
       when 1
-        [values.inject(1) { |acc, v| acc * v }, pointer]
+        [values.inject(1) { |acc, v| acc * v}, pointer]
       when 2
         [values.min, pointer]
       when 3
         [values.max, pointer]
       when 5
-        [values.inject(&:>) ? 1 : 0, pointer]
+        [values.first > values[1] ? 1 : 0, pointer]
       when 6
-        [values.inject(&:<) ? 1 : 0, pointer]
+        [values.first < values[1] ? 1 : 0, pointer]
       when 7
-        [values.inject(&:==) ? 1 : 0, pointer]
+        [values.first == values[1] ? 1 : 0, pointer]
       end
     end
   end
