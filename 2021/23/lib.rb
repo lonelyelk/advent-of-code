ee030c78887f28b242382f2101d135c9e7b24526
Sergey Kruk <sergey.kruk@gmail.com>
2021-12-25 22:14:00 +0100

    Add a slow solution of part 1 of day 23

---
+++ 2021/23/lib.rb
@@ -0,0 +1,146 @@
+# frozen_string_literal: true
+
+module Day23
+  HALL = [0, 1, 3, 5, 7, 9, 10].freeze
+  COSTS = {
+    A: 1,
+    B: 10,
+    C: 100,
+    D: 1000,
+  }.freeze
+  MOVES_OUT = {
+    2 => {
+      left: {
+        1 => 2,
+        0 => 3,
+      }.freeze,
+      right: {
+        3 => 2,
+        5 => 4,
+        7 => 6,
+        9 => 8,
+        10 => 9,
+      }.freeze,
+    },
+    4 => {
+      left: {
+        3 => 2,
+        1 => 4,
+        0 => 5,
+      }.freeze,
+      right: {
+        5 => 2,
+        7 => 4,
+        9 => 6,
+        10 => 7,
+      }.freeze,
+    },
+    6 => {
+      left: {
+        5 => 2,
+        3 => 4,
+        1 => 6,
+        0 => 7,
+      }.freeze,
+      right: {
+        7 => 2,
+        9 => 4,
+        10 => 5,
+      }.freeze,
+    },
+    8 => {
+      left: {
+        7 => 2,
+        5 => 4,
+        3 => 6,
+        1 => 8,
+        0 => 9,
+      }.freeze,
+      right: {
+        9 => 2,
+        10 => 3,
+      }.freeze,
+    },
+  }.freeze
+  HOMES = {
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8,
+  }.freeze
+
+  def process_input(str)
+    str.split("\n").each_with_object([Array.new(11)]) do |line, acc|
+      if (md = line.match(/#(\w)#(\w)#(\w)#(\w)#/))
+        acc.push(md[1, 4].map(&:to_sym))
+      end
+    end
+  end
+
+  def possible_moves(state)
+    possible = []
+    (1..2).each do |d|
+      state[d].each_with_index do |a, i|
+        next if a.nil?
+        next if d > 1 && !state[1][i].nil?
+
+        place = (i + 1) * 2
+        next if HOMES[a] == place && (d > 1 || HOMES[state[2][i]] == place)
+
+        [:left, :right].each do |dir|
+          MOVES_OUT[place][dir].each do |hall_pos, cost|
+            break unless state[0][hall_pos].nil?
+
+            next_hall = state[0].dup
+            next_hall[hall_pos] = a
+            next_states = state[1, 2].map(&:dup)
+            next_states[d - 1][i] = nil
+            possible.push([next_hall, *next_states, (cost + d - 1) * COSTS[a]])
+          end
+        end
+      end
+    end
+    state[0].each_with_index do |a, i|
+      next if a.nil?
+
+      home = HOMES[a]
+      pos = home / 2 - 1
+      next if !state[1][pos].nil?
+      next if state[2][pos] != a && !state[2][pos].nil?
+
+      path = state[0][([i + 1, home].min)..([i - 1, home].max)]
+      next if path.any?
+
+      cost = path.size + 1
+      d = state[2][pos].nil? ? 1 : 0
+      next_hall = state[0].dup
+      next_hall[i] = nil
+      next_states = state[1, 2].map(&:dup)
+      next_states[d][pos] = a
+      possible.push([next_hall, *next_states, (cost + d) * COSTS[a]])
+    end
+    possible
+  end
+
+  def find_solutions(input)
+    possible_moves(input).each_with_object([]) do |move, acc|
+      if move[0].all?(&:nil?)
+        acc.push move[3]
+      else
+        key = move[0..2].inject(&:+)
+        @cache[key] ||= find_solutions(move)
+        acc.concat(@cache[key].map { |c| c + move[3] })
+      end
+    end
+  end
+
+  def problem1(input)
+    @cache = {}
+    solutions = find_solutions(input)
+
+    solutions.min
+  end
+
+  def problem2(input)
+  end
+end
---
+++ spec/2021/23/day23_lib_spec.rb
@@ -0,0 +1,106 @@
+# frozen_string_literal: true
+
+require_relative "../../../2021/23/lib"
+
+RSpec.describe Day23 do
+  include described_class
+
+  let(:input) do
+    %(#############
+#...........#
+###B#C#B#D###
+  #A#D#C#A#
+  #########
+)
+  end
+  let(:processed_input) do
+    [
+      Array.new(11),
+      [:B, :C, :B, :D],
+      [:A, :D, :C, :A],
+    ]
+  end
+
+  describe "possible_moves" do
+    context "with move out moves" do
+      let(:state1) do
+        [
+          [nil, :B, nil, :C, nil, :D, nil, :A, nil, nil, nil],
+          Array.new(4),
+          [:A, :D, :C, :B],
+        ]
+      end
+
+      it "returns possible moves for initial state" do
+        expect(possible_moves(processed_input).size).to eq(28)
+      end
+
+      it "returns possible moves with costs" do
+        expect(possible_moves(processed_input).map(&:size)).to all(eq(4))
+      end
+
+      it "places one amphipod in a hall possible moves with costs" do
+        expect(possible_moves(processed_input).map { |st| st[0].count { |pos| !pos.nil? } }).to all(eq(1))
+      end
+
+      it "returns possible moves for almost all out state" do
+        expect(possible_moves(state1).size).to eq(2)
+      end
+
+      it "calculates costs of possible moves for almost all out state correctly" do
+        expect(possible_moves(state1).map { |st| st[3] }).to match_array([30, 40])
+      end
+    end
+
+    context "with move in moves" do
+      let(:state1) do
+        [
+          [nil, :A, nil, :B, nil, :D, nil, nil, nil, :C, nil],
+          Array.new(4),
+          [:A, :D, :C, :B],
+        ]
+      end
+      let(:state2) do
+        [
+          [nil, nil, nil, :B, nil, :D, nil, :C, nil, :C, nil],
+          [:A, nil, nil, nil],
+          [:A, :D, nil, :B],
+        ]
+      end
+
+      it "detects two amphipods that can return" do
+        expect(possible_moves(state1).size).to eq(3)
+      end
+
+      it "calculates cost of return" do
+        expect(possible_moves(state1).map { |st| st[3] }).to match_array([30, 2, 400])
+      end
+
+      it "detects one amphipod that can return" do
+        expect(possible_moves(state2).size).to eq(1)
+      end
+
+      it "calculates the cost of return for single amphipod" do
+        expect(possible_moves(state2).map { |st| st[3] }).to match_array([300])
+      end
+    end
+  end
+
+  describe "process_input" do
+    it "consumes multiline string and returns processed input" do
+      expect(process_input(input)).to eq(processed_input)
+    end
+  end
+
+  describe "problem1" do
+    it "returns nil 12_521 for test input" do
+      expect(problem1(processed_input)).to eq(12_521)
+    end
+  end
+
+  describe "problem2" do
+    it "returns nil for test input" do
+      expect(problem2(processed_input)).to eq(nil)
+    end
+  end
+end
