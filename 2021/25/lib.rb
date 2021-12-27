c20b168fbf6adbdd4c6ac33cbe991e3fa95fb718
Sergey Kruk <sergey.kruk@gmail.com>
2021-12-27 18:17:42 +0100

    Add solution to day 25

---
+++ 2021/25/lib.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+# https://adventofcode.com/2021/day/25
+module Day25
+  def process_input(str)
+    lines = str.split("\n").reject(&:empty?)
+    @height = lines.size
+    (0...@height).each_with_object(east: {}, south: {}) do |y, acc|
+      line = lines[y].chars
+      @width ||= line.size
+      (0...@width).each do |x|
+        case line[x]
+        when ">"
+          acc[:east][[x, y]] = true
+        when "v"
+          acc[:south][[x, y]] = true
+        end
+      end
+    end
+  end
+
+  def problem1(input)
+    state = input
+    frame = [@width, @height]
+    directions = [:east, :south]
+    (1..).each do |move|
+      next_state = { east: {}, south: {}}
+      moved = 0
+      directions.each_with_index do |dir, i|
+        state[dir].each do |coord, _|
+          new_coord = coord.dup
+          new_coord[i] += 1
+          new_coord[i] = 0 if new_coord[i] >= frame[i]
+          if (directions[i + 1] && state.any? { |_, places| places[new_coord] }) ||
+              (i > 0 && (state[directions[i]][new_coord] || next_state[directions[i - 1]][new_coord]))
+            next_state[dir][coord] = true
+          else
+            moved += 1
+            next_state[dir][new_coord] = true
+          end
+        end
+      end
+
+      return move if moved == 0
+
+      state = next_state
+    end
+  end
+end
---
+++ spec/2021/25/day25_lib_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require_relative "../../../2021/25/lib"
+
+RSpec.describe Day25 do
+  include described_class
+
+  let(:input) do
+    %(v...>>.vv>
+.vv>>.vv..
+>>.>v>...v
+>>v>>.>.v.
+v>v.vv.v..
+>.>>..v...
+.vv..>.>v.
+v.v..>>v.v
+....v..v.>
+)
+  end
+
+  describe "process_input" do
+    it "consumes multiline string and returns processed input" do
+      expect(process_input(input)).to match(a_hash_including(:east, :south))
+    end
+    it "places sea cucumbers facing east corretly" do
+      expect(process_input(input)[:east].size).to eq(23)
+    end
+    it "places sea cucumbers facing south corretly" do
+      expect(process_input(input)[:south].size).to eq(26)
+    end
+  end
+
+  describe "problem1" do
+    it "returns 58 for test input" do
+      expect(problem1(process_input(input))).to eq(58)
+    end
+  end
+end
