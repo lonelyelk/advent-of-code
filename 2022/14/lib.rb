7b759d7b01fd4b234e612b3182d7d04ffa111076
Sergey Kruk <sergey.kruk@gmail.com>
2022-12-14 19:12:48 +0100

    Advance in linting a bit

--- 2022/14/lib.rb
+++ 2022/14/lib.rb
@@ -4,35 +4,22 @@
 module Year2022
   module Day14
     class EndlessVoid < RuntimeError; end
-    class FullOfSand < RuntimeError; end
 
     def process_input(str)
-      parsed = Hash.new { |hsh, key| hsh[key] = {} }
-      str.split("\n").reject(&:empty?).each do |line|
-        pairs = line.split(" -> ").map { |pair| pair.split(",").map(&:to_i) }
-        pairs.each_with_index do |(x, y), i|
-          if i.zero?
-            parsed[x][y] = ?#
-          else
-            prev_x, prev_y = pairs[i - 1]
-            if x == prev_x
-              Range.new(*[y, prev_y].minmax).each do |yy|
-                parsed[x][yy] = ?#
-              end
-            else
-              Range.new(*[x, prev_x].minmax).each do |xx|
-                parsed[xx][y] = ?#
-              end
+      str.split("\n").reject(&:empty?).each_with_object(hash_2d) do |line, parsed|
+        split_to_pairs(line).each_cons(2) do |(prev_x, prev_y), (x, y)|
+          Range.new(*[y, prev_y].minmax).each do |yy|
+            Range.new(*[x, prev_x].minmax).each do |xx|
+              parsed[xx][yy] = ?#
             end
           end
         end
       end
-      parsed
     end
 
     def problem1(input)
       count = 0
-      while true
+      loop do
         drop([500, 0], input)
         count += 1
       end
@@ -41,19 +28,19 @@ module Year2022
     end
 
     def problem2(input)
-      count = 1
+      count = 0
       floor = input.values.map(&:keys).map(&:max).max + 2
-      while true
+      loop do
+        return count unless input[500][0].nil?
+
         drop_floor([500, 0], input, floor)
         count += 1
       end
-    rescue FullOfSand
-      count
     end
 
     def print_state(input)
       x_min, x_max = input.keys.minmax
-      y_min, y_max = input.values.map { |h| h.keys.minmax }.flatten.minmax
+      y_min, y_max = input.values.map(&:keys).flatten.minmax
       puts
       (y_min..y_max).each do |y|
         (x_min..x_max).each do |x|
@@ -65,38 +52,44 @@ module Year2022
 
     private
 
-    def drop(point, input)
-      stones_under = input[point[0]].keys.select { |k| k > point[1] }
-      raise EndlessVoid if stones_under.empty?
+    def hash_2d
+      Hash.new { |hsh, key| hsh[key] = {} }
+    end
 
-      y_pos = stones_under.min - 1
-      if input[point[0] - 1][y_pos + 1].nil?
-        drop([point[0] - 1, y_pos + 1], input)
-      elsif input[point[0] + 1][y_pos + 1].nil?
-        drop([point[0] + 1, y_pos + 1], input)
+    def split_to_pairs(line)
+      line.split(" -> ").map { |pair| pair.split(",").map(&:to_i) }
+    end
+
+    def drop(point, input)
+      stone_under = find_stone_under(point, input) || raise(EndlessVoid)
+
+      x = [point[0] - 1, point[0] + 1].detect { |xx| input[xx][stone_under].nil? }
+      if x
+        drop([x, stone_under], input)
       else
-        input[point[0]][y_pos] = ?o
+        input[point[0]][stone_under - 1] = ?o
         input
       end
     end
 
     def drop_floor(point, input, floor)
-      stones_under = input[point[0]].keys.select { |k| k > point[1] }
-      if stones_under.empty?
+      stone_under = find_stone_under(point, input)
+      if stone_under.nil?
         input[point[0]][floor - 1] = ?o
         return input
       end
-      y_pos = stones_under.min - 1
-      if input[point[0] - 1][y_pos + 1].nil?
-        drop_floor([point[0] - 1, y_pos + 1], input, floor)
-      elsif input[point[0] + 1][y_pos + 1].nil?
-        drop_floor([point[0] + 1, y_pos + 1], input, floor)
-      elsif y_pos > 0
-        input[point[0]][y_pos] = ?o
-        input
+
+      x = [point[0] - 1, point[0] + 1].detect { |xx| input[xx][stone_under].nil? }
+      if x
+        drop_floor([x, stone_under], input, floor)
       else
-        raise FullOfSand
+        input[point[0]][stone_under - 1] = ?o
+        input
       end
+    end
+
+    def find_stone_under(point, input)
+      input[point[0]].keys.sort.detect { |k| k > point[1] }
     end
   end
 end
