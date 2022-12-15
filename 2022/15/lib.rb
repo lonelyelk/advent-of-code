57d0468b22c7d5618ec6ecd7a2750a66d264f58d
Sergey Kruk <sergey.kruk@gmail.com>
2022-12-15 22:45:33 +0100

    Convince rubocop

--- 2022/15/lib.rb
+++ 2022/15/lib.rb
@@ -13,6 +13,7 @@ module Year2022
       end
     end
 
+    # rubocop:disable Metrics/AbcSize
     def problem1(input, target_y = 2_000_000)
       excluded_xs = input.each_with_object([]) do |line, excluded|
         dx = line[:distance] - (line[:sensor][1] - target_y).abs
@@ -20,13 +21,12 @@ module Year2022
 
         excluded.push((line[:sensor][0] - dx)..(line[:sensor][0] + dx))
       end
-      occupied_xs = input.each_with_object({}) do |line, acc|
-        acc[line[:beacon][0]] = true if line[:beacon][1] == target_y
-      end
       merged = merge_ranges(excluded_xs)
-      merged.map(&:size).inject(&:+) - occupied_xs.keys.count { |x| merged.any? { |r| r.include?(x) } }
+      merged.map(&:size).inject(&:+) -
+        occupied__by_beacon_xs(input, target_y).count { |x| merged.any? { |r| r.include?(x) } }
     end
 
+    # rubocop:disable Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
     def problem2(input, max_coord = 4_000_000)
       key_ys = input.each_with_object(input.map { |l| l[:sensor][1] }) do |line, ys|
         ys.push(line[:sensor][1] - line[:distance], line[:sensor][1] + line[:distance])
@@ -44,12 +44,15 @@ module Year2022
         mins, maxs = excluded_xs.map(&:minmax).transpose.map(&:uniq)
         gaps = mins.each_with_object([]) do |min, acc|
           maxs.each do |max|
-            acc.push([min - max, max + 1]) if min > max && (min - max) % 2 == 0
+            acc.push([min - max, max + 1]) if min > max && (min - max).even?
           end
         end
-        merged = merge_ranges(excluded_xs)
-        gaps.select { |(diff, _)| diff == 2 }.each do |(_, x)|
-          return x * 4_000_000 + target_y if merged.all? { |r| !r.cover?(x) }
+        candidates = gaps.select { |(diff, _)| diff == 2 }
+        unless candidates.empty?
+          merged = merge_ranges(excluded_xs)
+          candidates.each do |(_, x)|
+            return x * 4_000_000 + target_y if merged.all? { |r| !r.cover?(x) }
+          end
         end
         min_gap = gaps.min_by(&:first)
         if min_gap.nil?
@@ -60,8 +63,16 @@ module Year2022
         end
       end
     end
+    # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
 
     private
+
+    def occupied__by_beacon_xs(input, target_y)
+      occupied_xs = input.each_with_object({}) do |line, acc|
+        acc[line[:beacon][0]] = true if line[:beacon][1] == target_y
+      end
+      occupied_xs.keys
+    end
 
     def merge_ranges(ranges)
       ranges.sort { |a, b| a.min <=> b.min }.each_with_object([]) do |r, acc|
