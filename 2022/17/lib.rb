e2c05235819919b399d5d2f8b4ebdbd5abab37bc
Sergey Kruk <sergey.kruk@gmail.com>
2022-12-17 19:35:05 +0100

    Calm down rubocop for day 17

--- 2022/17/lib.rb
+++ 2022/17/lib.rb
@@ -17,39 +17,25 @@ module Year2022
       str.chomp
     end
 
+    # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
     def problem1(input, max_rocks = 2022)
       init!
       loop do
         dx = blow!(input)
-        new_x = @x + dx
-        if dx.positive?
-          if new_x + @rock.first.size <= WIDTH &&
-            @rock.each_with_index.all? { |l, i| @pipe[@y + i].nil? || @pipe[@y + i][new_x + l.rindex(?#)] != ?# }
-            @x = new_x
-          end
-        else
-          if new_x >= 0 &&
-            @rock.each_with_index.all? { |l, i| @pipe[@y + i].nil? || @pipe[@y + i][new_x + l.index(?#)] != ?# }
-            @x = new_x
-          end
-        end
-        if @y.positive? && (@pipe[@y - 1].nil? || @rock.each_with_index.all?{ |l, i| l.chars.each_with_index.all? { |c, n| @pipe[@y + i - 1].nil? || c != ?# || @pipe[@y + i - 1][@x + n] != ?# } })
+        @x += dx if x_move_allowed?(@x + dx)
+
+        if @y.positive? && (@pipe[@y - 1].nil? || move_allowed?(@x, @y - 1))
           @y -= 1
         else
-          @rock.each_with_index do |l, i|
-            @pipe[@y + i] ||= ?. * WIDTH
-            l.chars.each_with_index { |c, n| c == ?# && @pipe[@y + i][@x + n] = c }
-          end
+          land_rock!
           optimize!(max_rocks)
           @top = [@y + @rock.size, @top].max
           next_rock!
-          if @c_rocks > max_rocks
-            print_pipe(input)
-            return @top + @delta_top
-          end
+          return @top + @delta_top if @c_rocks > max_rocks
         end
       end
     end
+    # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 
     def problem2(input)
       problem1(input, 1_000_000_000_000)
@@ -69,51 +55,71 @@ module Year2022
     end
 
     def blow!(input)
-      out = input[@wind] == ?< ? -1 : 1
+      out = input[@wind %= input.size] == ?< ? -1 : 1
       @wind += 1
-      @wind %= input.size
       out
     end
 
     def next_rock!
-      @rock = ROCKS[@rock_index]
+      @rock = ROCKS[@rock_index %= ROCKS.size]
       @rock_index += 1
-      @rock_index %= ROCKS.size
       @c_rocks += 1
       @x = 2
       @y = @top + 3
     end
 
+    def x_move_allowed?(new_x)
+      new_x + @rock.first.size <= WIDTH && new_x >= 0 && move_allowed?(new_x, @y)
+    end
+
+    def move_allowed?(new_x, new_y)
+      @rock.each_with_index.all? do |line, i|
+        @pipe[new_y + i].nil? ||
+          line.chars.each_with_index.all? { |c, n| c != ?# || @pipe[new_y + i][new_x + n] != ?# }
+      end
+    end
+
+    def land_rock!
+      @rock.each_with_index do |l, i|
+        @pipe[@y + i] ||= ?. * WIDTH
+        l.chars.each_with_index { |c, n| c == ?# && @pipe[@y + i][@x + n] = c }
+      end
+    end
+
+    def magic_rock?
+      @delta_top.zero? && @rock_index == 1 && @y == @top && (1..2).cover?(@x)
+    end
+
     def optimize!(max_rocks)
-      return if @delta_top.positive? || @rock_index != 1 || @y != @top || !(1..2).cover?(@x)
+      return unless magic_rock?
 
       key = "#{@wind},#{@x}"
       if @cache[key]
         rocks_period = @c_rocks - @cache[key][:rocks]
         n_periods = (max_rocks - @c_rocks) / rocks_period
-        d_rocks = rocks_period * n_periods
-        if @c_rocks + d_rocks <= max_rocks
-          @c_rocks += d_rocks
-          @delta_top = (@top - @cache[key][:top]) * n_periods
-        end
+        @c_rocks += rocks_period * n_periods
+        @delta_top = (@top - @cache[key][:top]) * n_periods
       else
         @cache[key] = { top: @top, rocks: @c_rocks }
       end
     end
 
+    # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
     def print_pipe(input)
-      puts
-      puts input[@wind]
-      puts
-      (([@top + @rock.size + 3 - 30, 0].max)...(@top + @rock.size + 3)).to_a.reverse.each do |yy|
+      puts "\n#{input[@wind]}\n"
+      y_range_reversed.each do |yy|
         l = ?. * WIDTH
         if (0...@rock.size).include?(yy - @y)
           @rock[yy - @y].chars.each_with_index { |c, i| c == ?# && l[i + @x] = input[@wind] }
         end
-        @pipe[yy].chars.each_with_index { |c, i| c == ?# && l[i] = ?# } if @pipe[yy]
+        @pipe[yy]&.chars&.each_with_index { |c, i| c == ?# && l[i] = ?# }
         puts l
       end
-      puts
+    end
+    # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+
+    def y_range_reversed
+      (([@top + @rock.size + 3 - 30, 0].max)...(@top + @rock.size + 3)).to_a.reverse
     end
   end
 end
