b876f9ae1a54004b590f083c3133acfc82732ad7
Sergey Kruk <sergey.kruk@gmail.com>
2022-12-18 13:32:09 +0100

    Refactor and reuse

--- 2022/18/lib.rb
+++ 2022/18/lib.rb
@@ -22,36 +22,24 @@ module Year2022
     end
 
     def problem2(input)
-      total_sides = input.size * 6
+      total_sides = problem1(input)
       cover_x = []
       cover_y = []
       cover_z = []
       input.sort.each_cons(2) do |(x1, y1, z1), (x2, y2, z2)|
-        next unless x1 == x2 && y1 == y2
+        next unless x1 == x2 && y1 == y2 && z2 - z1 > 1
 
-        if z2 - z1 == 1
-          total_sides -= 2
-        else
-          cover_z.push(((z1 + 1)...z2).map { |z| [x1, y1, z] })
-        end
+        cover_z.push(((z1 + 1)...z2).map { |z| [x1, y1, z] })
       end
       input.sort_by { |(x, y, z)| [x, z, y] }.each_cons(2) do |(x1, y1, z1), (x2, y2, z2)|
-        next unless x1 == x2 && z1 == z2
+        next unless x1 == x2 && z1 == z2 && y2 - y1 > 1
 
-        if y2 - y1 == 1
-          total_sides -= 2
-        else
-          cover_y.push(((y1 + 1)...y2).map { |y| [x1, y, z1] })
-        end
+        cover_y.push(((y1 + 1)...y2).map { |y| [x1, y, z1] })
       end
       input.sort_by { |(x, y, z)| [y, z, x] }.each_cons(2) do |(x1, y1, z1), (x2, y2, z2)|
-        next unless z1 == z2 && y1 == y2
+        next unless z1 == z2 && y1 == y2 && x2 - x1 > 1
 
-        if x2 - x1 == 1
-          total_sides -= 2
-        else
-          cover_x.push(((x1 + 1)...x2).map { |x| [x, y1, z1] })
-        end
+        cover_x.push(((x1 + 1)...x2).map { |x| [x, y1, z1] })
       end
       flat_cover_y = cover_y.flatten(1)
       flat_cover_z = cover_z.flatten(1)
@@ -61,8 +49,7 @@ module Year2022
       flat_cover_y = cover_y.flatten(1)
       cover_z.select! { |pocket| pocket.all? { |cube| flat_cover_x.include?(cube) && flat_cover_y.include?(cube) } }
       air_cubes = cover_z.flatten(1)
-      total_sides -= problem1(air_cubes)
-      total_sides
+      total_sides - problem1(air_cubes)
     end
   end
 end
