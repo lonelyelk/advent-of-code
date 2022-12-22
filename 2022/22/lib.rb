229dd4f5cc7136c6bd6e8fe1168c0b95c2859919
Sergey Kruk <sergey.kruk@gmail.com>
2022-12-23 00:48:34 +0100

    Solve part 2 of day 23

--- 2022/22/lib.rb
+++ 2022/22/lib.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
+require_relative "nets"
+
 # https://adventofcode.com/2022/day/22
 module Year2022
   module Day22
+    include Nets
+
     def process_input(str)
       field, path = str.split("\n\n")
       board = {}
@@ -25,10 +29,21 @@ module Year2022
         dir *= rot == ?R ? 1i : -1i
         mv.to_i.times { pos = move_one(dir, pos, board) }
       end
-      1000 * (pos.imag + 1) + 4 * (pos. real + 1) + dir_to_pass(dir)
+      1000 * (pos.imag + 1) + 4 * (pos.real + 1) + dir_to_pass(dir)
     end
 
-    def problem2(input)
+    def problem2(input, test_net = false)
+      @net = test_net ? NET1 : NET2
+      board, path = input
+      @side = Math.sqrt(board.keys.size / 6).to_i
+      dir = 1 + 0i
+      pos = board.keys.select { |coord| coord.imag.zero? }.min_by(&:real)
+      path.scan(/(\d+)?([RL])(\d+)/).each do |(pre, rot, mv)|
+        pre.to_i.times { dir, pos = move_one_cube(dir, pos, board) } if pre
+        dir *= rot == ?R ? 1i : -1i
+        mv.to_i.times { dir, pos = move_one_cube(dir, pos, board) }
+      end
+      1000 * (pos.imag + 1) + 4 * (pos.real + 1) + dir_to_pass(dir)
     end
 
     private
@@ -40,6 +55,16 @@ module Year2022
         maybe_next_pos
       when ?#
         pos
+      end
+    end
+
+    def move_one_cube(dir, pos, board)
+      maybe_next_dir, maybe_next_pos = next_pos_cube(dir, pos, board)
+      case board[maybe_next_pos]
+      when ?.
+        [maybe_next_dir, maybe_next_pos]
+      when ?#
+        [dir, pos]
       end
     end
 
@@ -56,6 +81,13 @@ module Year2022
       when -1i
         board.keys.select { |coord| coord.real == pos.real }.max_by(&:imag)
       end
+    end
+
+    def next_pos_cube(dir, pos, board)
+      return [dir, pos + dir] if board.key?(pos + dir)
+
+      face = (dir.real.zero? ? pos.real : pos.imag).to_i / @side
+      [dir * @net[dir][face][:rot], @net[dir][face][:trans][pos, @side]]
     end
 
     def dir_to_pass(dir)
---
+++ 2022/22/nets.rb
@@ -0,0 +1,136 @@
+# frozen_string_literal: true
+
+module Nets
+  NET1 = {
+    -1 + 0i => {
+      0 => {
+        trans: ->(n, side) { (side + n.imag) + side * 1i },
+        rot: -1i,
+      },
+      1 => {
+        trans: ->(n, side) { (5 * side - 1 - n.imag) + (3 * side - 1) * 1i },
+        rot: 1i,
+      },
+      2 => {
+        trans: ->(n, side) { (4 * side - 1 - n.imag) + (2 * side - 1) * 1i },
+        rot: 1i,
+      },
+    },
+    1 + 0i => {
+      0 => {
+        trans: ->(n, side) { (4 * side - 1) + (side * 3 - 1 - n.imag) * 1i },
+        rot: -1,
+      },
+      1 => {
+        trans: ->(n, side) { (5 * side - 1 - n.imag) + side * 2i },
+        rot: 1i,
+      },
+      2 => {
+        trans: ->(n, side) { (3 * side - 1) + (3 * side - 1 - n.imag) * 1i },
+        rot: -1,
+      },
+    },
+    -1i => {
+      0 => {
+        trans: ->(n, side) { (3 * side - 1 - n.real) + 0i },
+        rot: -1,
+      },
+      1 => {
+        trans: ->(n, side) { 2 * side + (n.real - side) * 1i },
+        rot: 1i,
+      },
+      2 => {
+        trans: ->(n, side) { (3 * side - 1 - n.real) + side * 1i },
+        rot: -1,
+      },
+      3 => {
+        trans: ->(n, side) { (3 * side - 1) + (5 * side - 1 - n.real) * 1i },
+        rot: -1i,
+      }
+    },
+    1i => {
+      0 => {
+        trans: ->(n, side) { (3 * side - 1 - n.real) + (3 * side - 1) * 1i },
+        rot: -1,
+      },
+      1 => {
+        trans: ->(n, side) { 2 * side + (4 * side - 1 - n.real) * 1i },
+        rot: -1i,
+      },
+      2 => {
+        trans: ->(n, side) { (3 * side - 1 - n.real) + (2 * side - 1) * 1i },
+        rot: -1,
+      },
+      3 => {
+        trans: ->(n, side) { 0 + (5 * side - 1 - n.real) * 1i },
+        rot: -1i,
+      }
+    },
+  }
+  NET2 = {
+    -1 + 0i => {
+      0 => {
+        trans: ->(n, side) { 0 + (3 * side - 1 - n.imag) * 1i },
+        rot: -1,
+      },
+      1 => {
+        trans: ->(n, side) { (n.imag - side) + side * 2i },
+        rot: -1i,
+      },
+      2 => {
+        trans: ->(n, side) { side + (3 * side - 1 - n.imag) * 1i },
+        rot: -1,
+      },
+      3 => {
+        trans: ->(n, side) { (n.imag - 2 * side) + 0i },
+        rot: -1i,
+      },
+    },
+    1 + 0i => {
+      0 => {
+        trans: ->(n, side) { (2 * side - 1) + (3 * side - 1 - n.imag) * 1i },
+        rot: -1,
+      },
+      1 => {
+        trans: ->(n, side) { n.imag + side + (side - 1) * 1i },
+        rot: -1i,
+      },
+      2 => {
+        trans: ->(n, side) { (3 * side - 1) + (3 * side - 1 - n.imag) * 1i },
+        rot: -1,
+      },
+      3 => {
+        trans: ->(n, side) { (n.imag - 2 * side) + (3 * side - 1) * 1i },
+        rot: -1i,
+      }
+    },
+    -1i => {
+      0 => {
+        trans: ->(n, side) { side + (side + n.real) * 1i },
+        rot: 1i,
+      },
+      1 => {
+        trans: ->(n, side) { 0 + (n.real + 2 * side) * 1i },
+        rot: 1i,
+      },
+      2 => {
+        trans: ->(n, side) { (n.real - 2 * side) + (4 * side - 1) * 1i },
+        rot: 1,
+      },
+    },
+    1i => {
+      0 => {
+        trans: ->(n, side) { (2 * side + n.real) + 0i },
+        rot: 1,
+      },
+      1 => {
+        trans: ->(n, side) { (side - 1) +  (2 * side + n.real) * 1i },
+        rot: 1i,
+      },
+      2 => {
+        trans: ->(n, side) { 2 * side - 1 + (n.real - side) * 1i },
+        rot: 1i,
+      }
+    }
+  }
+end
