389f55a1b86f7166d1d49b5f8ae0ec21d09adaa2
Sergey Kruk <sergey.kruk@gmail.com>
2023-12-01 15:17:00 +0100

    Refactor day 1 of 2023

--- 2023/01/lib.rb
+++ 2023/01/lib.rb
@@ -4,33 +4,40 @@
 module Year2023
   module Day01
     DIGITS = {
-      "one" => "1",
-      "two" => "2",
-      "three" => "3",
-      "four" => "4",
-      "five" => "5",
-      "six" => "6",
-      "seven" => "7",
-      "eight" => "8",
-      "nine" => "9",
-      "zero" => "0",
+      "one" => 1,
+      "two" => 2,
+      "three" => 3,
+      "four" => 4,
+      "five" => 5,
+      "six" => 6,
+      "seven" => 7,
+      "eight" => 8,
+      "nine" => 9,
+      "zero" => 0,
     }.freeze
+
     def process_input(str)
       str.split("\n").reject(&:empty?)
     end
 
     def problem1(input)
       input.sum do |line|
-        digits = line.scan(/\d/)
-        (digits.first + digits.last).to_i
+        digits = line.scan(/\d/).map(&:to_i)
+        calibration_value(digits)
       end
     end
 
     def problem2(input)
       input.sum do |line|
-        digits = line.scan(/\d|#{DIGITS.keys.join("|")}/).map { |d| DIGITS[d] || d }
-        (digits.first + digits.last).to_i
+        digits = line.scan(/\d|#{DIGITS.keys.join("|")}/).map { |d| DIGITS[d] || d.to_i }
+        calibration_value(digits)
       end
+    end
+
+  private
+
+    def calibration_value(digits)
+      digits.first * 10 + digits.last
     end
   end
 end
