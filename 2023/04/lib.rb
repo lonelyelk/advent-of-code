358e0f4b6bee5f528bbd829a6195a1b0788684a5
Sergey Kruk <sergey.kruk@gmail.com>
2023-12-04 12:11:46 +0100

    Refactor to make robocop happy about day 4 of AoC 2023

--- 2023/04/lib.rb
+++ 2023/04/lib.rb
@@ -12,25 +12,29 @@ module Year2023
     def problem1(input)
       input.sum do |(winning, numbers)|
         won = winning.count { |num| numbers.include?(num) }
-        won.positive? ? 2 ** (won - 1) : 0
+        won.positive? ? 2**(won - 1) : 0
       end
     end
 
     def problem2(input)
-      cards = {}
+      cards = init_cards(input)
       input.each_with_index do |(winning, numbers), index|
-        cards[index] ||= 1
         won = winning.count { |num| numbers.include?(num) }
         next if won.zero?
 
         (1..won).each do |inc|
-          break if index + inc >= input.size
+          break unless cards[index + inc]
 
-          cards[index + inc] ||= 1
           cards[index + inc] += cards[index]
         end
       end
-      cards.values.sum
+      cards.sum
+    end
+
+    private
+
+    def init_cards(input)
+      Array.new(input.size, 1)
     end
   end
 end
