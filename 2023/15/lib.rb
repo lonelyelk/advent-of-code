d61f67c3f138acda1e24b6f9c0f125038a29b91d
Sergey Kruk <sergey.kruk@gmail.com>
2023-12-15 11:51:16 +0100

    Add solution to day 15 of AoC 2023

---
+++ 2023/15/lib.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+# https://adventofcode.com/2023/day/15
+module Year2023
+  module Day15
+    def process_input(str)
+      str.chomp
+    end
+
+    def problem1(input)
+      input.split(",").sum do |instr|
+        hash(instr)
+      end
+    end
+
+    def problem2(input)
+      state = Array.new(256) { |_i| [] }
+      state = input.split(",").each_with_object(state) do |instr, s|
+        md = instr.match(/(.+)([=-])(\d*)/)
+        label = md[1]
+        box = hash(label)
+        if md[2] == "-"
+          s[box].reject! { |l| l.first == label }
+        else
+          if (index = s[box].index { |l| l.first == label })
+            s[box][index] = [label, md[3].to_i]
+          else
+            s[box].push([label, md[3].to_i])
+          end
+        end
+      end
+      state.each_with_index.sum do |box, index|
+        box.each_with_index.sum do |(_l, fl), pos|
+          (index + 1) * (pos + 1) * fl
+        end
+      end
+    end
+
+    private
+
+    def hash(str)
+      str.chars.inject(0) do |acc, c|
+        (acc + c.ord) * 17 % 256
+      end
+    end
+  end
+end
---
+++ spec/2023/15/day15_lib_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require_relative "../../../2023/15/lib"
+
+RSpec.describe Year2023::Day15 do
+  include described_class
+
+  let(:input) do
+    %(rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
+)
+  end
+  let(:processed_input) do
+    "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+  end
+
+  describe "process_input" do
+    it "consumes multiline string and returns processed input" do
+      expect(process_input(input)).to eq(processed_input)
+    end
+  end
+
+  describe "problem1" do
+    it "returns 1320 for test input" do
+      expect(problem1(processed_input)).to eq(1320)
+    end
+  end
+
+  describe "problem2" do
+    it "returns 145 for test input" do
+      expect(problem2(processed_input)).to eq(145)
+    end
+  end
+end
