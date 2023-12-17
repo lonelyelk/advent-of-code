6bd4199a1a3cc28378531dedf459861c69cc5eef
Sergey Kruk <sergey.kruk@gmail.com>
2023-12-17 15:07:07 +0100

    Make day 17 of AoC 2023 faster

--- 2023/17/lib.rb
+++ 2023/17/lib.rb
@@ -10,29 +10,18 @@ module Year2023
     def problem1(input)
       path = Array.new(input.size) { |i| Array.new(input[i].size) { |_| {} } }
       path[0][0] = { [0, 0, 0, 0] => 0 }
-      processed = {}
-      changed = true
-      while changed
-        changed = false
+      to_process = { [0, 0, [0, 0, 0, 0]] => 0 }
+      until to_process.empty?
         target_heat = path[input.size - 1][input.first.size - 1].values.min
-        path.size.times do |y|
-          path[y].size.times do |x|
-            path[y][x].each do |len, heat|
-              next if processed[[y, x, len, heat]]
+        to_process = to_process.each_with_object({}) do |((y, x, len), heat), tp|
+          positions_with_index(x, y, input, len).each do |(yy, xx), i|
+            pos_len = 4.times.map { |ii| ii == i ? len[i] + 1 : 0 }
+            pos_heat = heat + input[yy][xx]
+            next if target_heat && pos_heat > target_heat
 
-              positions_with_index(x, y, input, len).each do |(yy, xx), i|
-                next if i.nil?
-
-                pos_len = 4.times.map { |ii| ii == i ? len[i] + 1 : 0}
-                pos_heat = heat + input[yy][xx]
-                next if target_heat && pos_heat > target_heat
-
-                if path[yy][xx][pos_len].nil? || path[yy][xx][pos_len] > pos_heat
-                  path[yy][xx][pos_len] = pos_heat
-                  changed = true
-                end
-              end
-              processed[[y, x, len, heat]] = true
+            if path[yy][xx][pos_len].nil? || path[yy][xx][pos_len] > pos_heat
+              path[yy][xx][pos_len] = pos_heat
+              tp[[yy, xx, pos_len]] = pos_heat
             end
           end
         end
@@ -44,29 +33,18 @@ module Year2023
     def problem2(input)
       path = Array.new(input.size) { |i| Array.new(input[i].size) { |_| {} } }
       path[0][0] = { [0, 0, 0, 0] => 0 }
-      processed = {}
-      changed = true
-      while changed
-        changed = false
+      to_process = { [0, 0, [0, 0, 0, 0]] => 0 }
+      until to_process.empty?
         target_heat = ultra_heat_loss(path)
-        path.size.times do |y|
-          path[y].size.times do |x|
-            path[y][x].each do |len, heat|
-              next if processed[[y, x, len, heat]]
+        to_process = to_process.each_with_object({}) do |((y, x, len), heat), tp|
+          ultra_positions_with_index(x, y, input, len).each do |(yy, xx), i|
+            pos_len = 4.times.map { |ii| ii == i ? len[i] + 1 : 0 }
+            pos_heat = heat + input[yy][xx]
+            next if target_heat && pos_heat > target_heat
 
-              ultra_positions_with_index(x, y, input, len).each do |(yy, xx), i|
-                next if i.nil?
-
-                pos_len = 4.times.map { |ii| ii == i ? len[i] + 1 : 0}
-                pos_heat = heat + input[yy][xx]
-                next if target_heat && pos_heat > target_heat
-
-                if path[yy][xx][pos_len].nil? || path[yy][xx][pos_len] > pos_heat
-                  path[yy][xx][pos_len] = pos_heat
-                  changed = true
-                end
-              end
-              processed[[y, x, len, heat]] = true
+            if path[yy][xx][pos_len].nil? || path[yy][xx][pos_len] > pos_heat
+              path[yy][xx][pos_len] = pos_heat
+              tp[[yy, xx, pos_len]] = pos_heat
             end
           end
         end
@@ -78,12 +56,8 @@ module Year2023
 
     def positions_with_index(x, y, input, len)
       [[y - 1, x], [y, x + 1], [y + 1, x], [y, x - 1]].each_with_index.map do |(yy, xx), i|
-        if len[i] >= 3 || len[(i + 2) % 4].positive? || outside?(xx, yy, input)
-          [[], nil]
-        else
-          [[yy, xx], i]
-        end
-      end
+        [[yy, xx], i] unless len[i] >= 3 || len[(i + 2) % 4].positive? || outside?(xx, yy, input)
+      end.compact
     end
 
     def outside?(x, y, input)
@@ -94,12 +68,10 @@ module Year2023
       current_dir = len.index(&:positive?)
       no_turn = current_dir && len[current_dir] < 4
       [[y - 1, x], [y, x + 1], [y + 1, x], [y, x - 1]].each_with_index.map do |(yy, xx), i|
-        if (i != current_dir && no_turn) || len[i] >= 10 || len[(i + 2) % 4].positive? || outside?(xx, yy, input)
-          [[], nil]
-        else
+        unless (i != current_dir && no_turn) || len[i] >= 10 || len[(i + 2) % 4].positive? || outside?(xx, yy, input)
           [[yy, xx], i]
         end
-      end
+      end.compact
     end
 
     def ultra_heat_loss(path)
