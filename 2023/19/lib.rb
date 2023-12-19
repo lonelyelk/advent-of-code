bc9b60e6293c46c114eabf1d26008b651749902b
Sergey Kruk <sergey.kruk@gmail.com>
2023-12-19 11:51:55 +0100

    Add solution to day 19 part 1 of AoC 2023

---
+++ 2023/19/lib.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+# https://adventofcode.com/2023/day/19
+module Year2023
+  module Day19
+    Part = Struct.new(:x, :m, :a, :s) do |_c|
+      def b
+        binding
+      end
+    end
+    def process_input(str)
+      workflows, ratings = str.split("\n\n").map { |section| section.split("\n") }
+      ratings = ratings.map do |l|
+        md = l.match(/x=(\d+),m=(\d+),a=(\d+),s=(\d+)/)
+        Part.new(x: md[1].to_i, m: md[2].to_i, a: md[3].to_i, s: md[4].to_i)
+      end
+      workflows = workflows.each_with_object({}) do |l, wf|
+        md = l.match(/(\w+)\{(.+)\}/)
+        wf[md[1]] = md[2].split(",").map { |op| op.split(":") }
+      end
+      { workflows:, ratings: }
+    end
+
+    def problem1(input)
+      input[:ratings].sum do |r|
+        op = process_step(input[:workflows], "in", r)
+        until %w[A R].include?(op)
+          op = process_step(input[:workflows], op, r)
+        end
+        op == "A" ? r.sum : 0
+      end
+    end
+
+    def problem2(input)
+    end
+
+    def process_step(workflows, operation, rating)
+      workflows[operation].each do |wf|
+        return wf.first if wf.size == 1
+        return wf[1] if rating.b.eval(wf[0])
+      end
+    end
+  end
+end
---
+++ spec/2023/19/day19_lib_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require_relative "../../../2023/19/lib"
+
+RSpec.describe Year2023::Day19 do
+  include described_class
+
+  let(:input) do
+    %(px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}
+)
+  end
+  let(:processed_input) do
+    nil
+  end
+
+  xdescribe "process_input" do
+    it "consumes multiline string and returns processed input" do
+      expect(process_input(input)).to eq(processed_input)
+    end
+  end
+
+  describe "problem1" do
+    it "returns 19114 for test input" do
+      expect(problem1(process_input(input))).to eq(19114)
+    end
+  end
+
+  describe "problem2" do
+    it "returns nil for test input" do
+      expect(problem2(process_input(input))).to eq(nil)
+    end
+  end
+end
