c96ab102dd4f25815384b460f334bfcdde811afd
Sergey Kruk <sergey.kruk@gmail.com>
2024-12-02 22:34:59 +0100

    Refactor day 2 of AoC 2024

--- 2024/02/lib.rb
+++ 2024/02/lib.rb
@@ -9,34 +9,26 @@ module Year2024
 
     def problem1(input)
       input.count do |report|
-        diffs = []
-        report[..-2].each_with_index do |num, i|
-          diffs.push(report[i + 1] - num)
+        diffs = report.each_cons(2).map do |prev, nxt|
+          nxt - prev
         end
-        safe?(diffs.sort)
+        safe?(diffs)
       end
     end
 
     def problem2(input)
       input.count do |report|
-        diffs = []
-        report[..-2].each_with_index do |num, i|
-          diffs.push(report[i + 1] - num)
-        end
-        safe?(diffs.sort) ||
-          report.size.times.any? do |i|
-            diffs = []
-            r = report.dup
-            r.delete_at(i)
-            r[..-2].each_with_index do |n, j|
-              diffs.push(r[j + 1] - n)
-            end
-            safe?(diffs.sort)
+        report.size.downto(0).any? do |i|
+          diffs = (report[...i] + (report[(i + 1)..] || [])).each_cons(2).map do |prev, nxt|
+            nxt - prev
           end
+          safe?(diffs)
+        end
       end
     end
 
-    def safe?(sorted_report)
+    def safe?(report)
+      sorted_report = report.sort
       (sorted_report.first.positive? && sorted_report.last <= 3) ||
         (sorted_report.first >= -3 && sorted_report.last.negative?)
     end
