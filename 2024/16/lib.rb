4c31d0fcdc2de9c0a350f81edc18b5c2b26e0da8
Sergey Kruk <sergey.kruk@gmail.com>
2024-12-16 22:33:51 +0100

    Negotiate with rubocop

--- 2024/16/lib.rb
+++ 2024/16/lib.rb
@@ -4,124 +4,80 @@
 module Year2024
   module Day16
     def process_input(str)
-      s = nil
-      e = nil
+      res = {}
       map = str.split("\n").map.with_index do |line, y|
         line.chars.map.with_index do |c, x|
-          if c == "S"
-            s = Complex(x, y)
-            "."
-          elsif c == "E"
-            e = Complex(x, y)
-            "."
-          else
-            c
-          end
+          res[:s] = Complex(x, y) if c == "S"
+          res[:e] = Complex(x, y) if c == "E"
+          c.sub(/[SE]/, ".")
         end.join
       end
-      { map:, s:, e: }
+      res.merge(map:)
     end
 
     def problem1(input)
-      map = input[:map].each_with_object({}).with_index do |(line, acc), y|
-        line.chars.each_with_index do |c, x|
-          next unless c == "."
-
-          c = Complex(x, y)
-          acc[c] = {}
-        end
-      end
-      map[input[:s]] = {
-        Complex(1, 0) => 0,
-        Complex(0, 1) => 1000,
-        Complex(0, -1) => 1000,
-        Complex(-1, 0) => 2000,
-      }
-      edge = [input[:s]]
-      until edge.empty?
-        edge = edge.each_with_object([]) do |c, acc|
-          map[c].each do |dir, pts|
-            pos = c + dir
-            next unless map[pos]
-
-            better = false
-            {
-              dir => pts + 1,
-              (dir * 1i) => pts + 1001,
-              (-dir * 1i) => pts + 1001,
-            }.each do |d, p|
-              if map[pos][d].nil? || map[pos][d] > p
-                better = true
-                map[pos][d] = p
-              end
-            end
-            acc.push(pos) if better
-          end
-        end
-      end
-      map[input[:e]].values.min
+      map = map_points(input)
+      map[input[:e]].values.map(&:first).min
     end
 
+    # rubocop:disable Metrics/AbcSize
     def problem2(input)
-      map = input[:map].each_with_object({}).with_index do |(line, acc), y|
+      map = map_points(input)
+      path = { input[:e] => map[input[:e]].values.min_by(&:first) }
+      until path[input[:s]]
+        path.merge!(path.each_with_object({}) do |(prev_c, (_, *coords)), acc|
+          coords.each { |c| acc[c] = map[c][prev_c - c] }
+        end)
+      end
+      path.size
+    end
+    # rubocop:enable Metrics/AbcSize
+
+    def transform_to_map(input)
+      input[:map].each_with_object({}).with_index do |(line, acc), y|
         line.chars.each_with_index do |c, x|
           next unless c == "."
 
           c = Complex(x, y)
-          acc[c] = {}
+          acc[c] = c == input[:s] ? { 1 + 0i => [0], 1i => [1000], -1i => [1000] } : {}
         end
       end
-      map[input[:s]] = {
-        Complex(1, 0) => [0],
-        Complex(0, 1) => [1000],
-        Complex(0, -1) => [1000],
-        Complex(-1, 0) => [2000],
+    end
+
+    def dir_options(dir, pts)
+      {
+        dir => pts + 1,
+        (1i * dir) => pts + 1001,
+        (-1i * dir) => pts + 1001,
       }
+    end
+
+    # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
+    def map_points(input)
+      map = transform_to_map(input)
       edge = [input[:s]]
       until edge.empty?
         edge = edge.each_with_object([]) do |c, acc|
-          map[c].each do |dir, pts_arr|
-            pts, = pts_arr
+          map[c].each do |dir, (pts, *_)|
             pos = c + dir
             next unless map[pos]
 
             better = false
-            {
-              dir => pts + 1,
-              (dir * 1i) => pts + 1001,
-              (-dir * 1i) => pts + 1001,
-            }.each do |d, p|
+            opts = pos == input[:e] ? { dir => pts + 1 } : dir_options(dir, pts)
+            opts.each do |d, p|
               if map[pos][d].nil? || map[pos][d].first > p
                 better = true
                 map[pos][d] = [p, c]
               elsif map[pos][d].first == p
-                map[pos][d].push(c)
+                map[pos][d] |= [c]
               end
             end
-            acc.push(pos) if better
+            acc.push(pos) if better && pos != input[:e]
           end
         end
       end
-      e = map[input[:e]].values.min_by(&:first)
-      edge = [[input[:e], *e]]
-      points = [input[:e]]
-      until edge.empty?
-        edge = edge.each_with_object([]) do |pts_arr, acc|
-          prev_c, pts, *coords = pts_arr
-          coords.uniq.each do |c|
-            points.push(c)
-            n_pts_arr = map[c][prev_c - c]
-            next unless n_pts_arr
-
-            acc.push([c, *n_pts_arr]) if [1, 1001].include?(pts - n_pts_arr.first)
-          end
-        end.uniq
-      end
-      points.uniq.size
+      map
     end
-
-    def map_char(map, coord)
-      map[coord.imag][coord.real]
-    end
+    # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
   end
 end
