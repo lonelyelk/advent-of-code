ae18a76d88f0c7f75c50efdec058c1fa47df8580
Sergey Kruk <sergey.kruk@gmail.com>
2025-12-01 13:05:30 +0100

    Minor refactor

--- 2025/01/lib.rb
+++ 2025/01/lib.rb
@@ -7,30 +7,30 @@ module Year2025
       "L" => -1,
       "R" => 1,
     }.freeze
+    START = 50
+    MAX = 100
 
     def process_input(str)
       str.split
     end
 
     def problem1(input)
-      curr = 50
-      max = 100
+      curr = START
       input.count do |move|
         dir = move[0]
         num = move[1..].to_i
-        curr = (curr + num * DIRECTION[dir]) % max
+        curr = (curr + num * DIRECTION[dir]) % MAX
         curr.zero?
       end
     end
 
     def problem2(input)
-      curr = 50
-      max = 100
+      curr = START
       input.sum do |move|
         dir = move[0]
         num = move[1..].to_i
         num.times.count do
-          curr = (curr + DIRECTION[dir]) % max
+          curr = (curr + DIRECTION[dir]) % MAX
           curr.zero?
         end
       end
