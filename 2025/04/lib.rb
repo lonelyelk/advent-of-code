18c81a3c5c999b6f9f991e67f924bfeaf4183892
Sergey Kruk <sergey.kruk@gmail.com>
2025-12-04 10:50:07 +0100

    Make linter slightly happier

--- 2025/04/lib.rb
+++ 2025/04/lib.rb
@@ -13,49 +13,43 @@ module Year2025
     end
 
     def problem1(input)
-      input.count do |(x, y), c|
-        c == "@" && adjacent(x, y).count { |point| input[point] == "@" } < 4
+      input.count do |point, c|
+        c == "@" && adjacent(point).count { |point| input[point] == "@" } < 4
       end
     end
 
     def problem2(input)
       count = 0
-      next_input = input
       loop do
-        next_count = count
-        next_input = next_input.each_with_object({}) do |((x, y), c), acc|
-          if c == "."
-            acc[[x, y]] = c
+        diff = 0
+        input = input.each_with_object({}) do |(point, c), acc|
+          if c == "." || count_adj_rolls(point, input) >= 4
+            acc[point] = c
           else
-            adj_rolls = adjacent(x, y).count { |point| next_input[point] == "@" }
-            if adj_rolls < 4
-              next_count += 1
-              acc[[x, y]] = "."
-            else
-              acc[[x, y]] = "@"
-            end
+            diff += 1 if c == "@"
+            acc[point] = "."
           end
         end
-        break if next_count == count
+        break if diff.zero?
 
-        count = next_count
+        count += diff
       end
       count
     end
 
     private
 
-    def adjacent(x, y)
+    def adjacent(point)
+      x, y = point
       [
-        [x - 1, y - 1],
-        [x, y - 1],
-        [x + 1, y - 1],
-        [x - 1, y],
-        [x + 1, y],
-        [x - 1, y + 1],
-        [x, y + 1],
-        [x + 1, y + 1],
+        [x - 1, y - 1], [x, y - 1], [x + 1, y - 1],
+        [x - 1, y], [x + 1, y],
+        [x - 1, y + 1], [x, y + 1], [x + 1, y + 1],
       ]
+    end
+
+    def count_adj_rolls(point, input)
+      adjacent(point).count { |pt| input[pt] == "@" }
     end
   end
 end
