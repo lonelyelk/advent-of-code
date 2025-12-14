a9baa59835f458e1a69eae5aa83492fcad635918
Sergey Kruk <sergey.kruk@gmail.com>
2025-12-14 18:02:42 +0100

    Make rubocop happy about day 9 of AoC 2025

--- 2025/09/lib.rb
+++ 2025/09/lib.rb
@@ -9,56 +9,68 @@ module Year2025
 
     def problem1(input)
       max = 0
-      (0...(input.length - 1)).each do |i|
-        ((i + 1)...input.length).each do |j|
-          area = input[i].zip(input[j]).map { |(a, b)| (a - b).abs + 1 }.inject(&:*)
-          max = area if area > max
-        end
+      input.combination(2).each do |pair|
+        area = calculate_area(pair)
+        max = area if area > max
       end
       max
     end
 
     def problem2(input)
-      edges = find_edges(input)
+      horizontals, verticals = find_edges(input)
       max = 0
-      (0...(input.length - 1)).each do |i|
-        ((i + 1)...input.length).each do |j|
-          area = input[i].zip(input[j]).map { |(a, b)| (a - b).abs + 1 }.inject(&:*)
-          next if area <= max
+      input.combination(2).each do |pair|
+        area = calculate_area(pair)
+        next if area <= max
+        next if inbound_horizontals(pair, horizontals)
+        next if inbound_verticals(pair, verticals)
 
-          ymin, ymax = [input[i].last, input[j].last].minmax
-          xmin, xmax = [input[i].first, input[j].first].minmax
-
-          cross = edges[:horizontals].any? do |(y, er)|
-            ((ymin + 1)..(ymax - 1)).include?(y) &&
-              ((er.include?(xmin) && er.end > xmin) ||
-              (er.include?(xmax) && er.begin < xmax))
-          end
-          next if cross
-
-          cross = edges[:verticals].any? do |(x, er)|
-            ((xmin + 1)..(xmax - 1)).include?(x) &&
-              ((er.include?(ymax) && er.begin < ymax) ||
-                (er.include?(ymin) && er.end > ymin))
-          end
-          next if cross
-
-          max = area if area > max
-        end
+        max = area if area > max
       end
       max
     end
 
     private
 
+    def calculate_area(pair)
+      pair.inject(&:zip).map { |(a, b)| (a - b).abs + 1 }.inject(&:*)
+    end
+
     def find_edges(input)
-      [*input, input.first].each_cons(2).with_object({ verticals: [], horizontals: [] }) do |(a, b), acc|
+      horizontals = []
+      verticals = []
+      [*input, input.first].each_cons(2) do |(a, b)|
         if a.first == b.first
-          acc[:verticals].push([a.first, Range.new(*[a.last, b.last].minmax)])
+          verticals.push([a.first, Range.new(*[a.last, b.last].minmax)])
         else
-          acc[:horizontals].push([a.last, Range.new(*[a.first, b.first].minmax)])
+          horizontals.push([a.last, Range.new(*[a.first, b.first].minmax)])
         end
       end
+      [horizontals, verticals]
     end
+
+    # rubocop:disable Metrics/CyclomaticComplexity
+    def inbound_horizontals(pair, horizontals)
+      ymin, ymax = pair.map(&:last).minmax
+      xmin, xmax = pair.map(&:first).minmax
+
+      horizontals.any? do |(y, er)|
+        ((ymin + 1)..(ymax - 1)).include?(y) &&
+          ((er.include?(xmin) && er.end > xmin) ||
+          (er.include?(xmax) && er.begin < xmax))
+      end
+    end
+
+    def inbound_verticals(pair, verticals)
+      ymin, ymax = pair.map(&:last).minmax
+      xmin, xmax = pair.map(&:first).minmax
+
+      verticals.any? do |(x, er)|
+        ((xmin + 1)..(xmax - 1)).include?(x) &&
+          ((er.include?(ymax) && er.begin < ymax) ||
+            (er.include?(ymin) && er.end > ymin))
+      end
+    end
+    # rubocop:enable Metrics/CyclomaticComplexity
   end
 end
